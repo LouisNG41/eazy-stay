edb61d1e63e11a9c4559f3decb9f68c2e216baab
Fenyx <64859587+steven-van@users.noreply.github.com>
2024-05-17 16:26:03 +0200

    fix: replace Logement by Property in insert

--- routes/properties.js
+++ routes/properties.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
 
 router.post("/add-property", (req, res) => {
   const {desc, adress, budget, type, photo} = req.body;
-  const query = `INSERT INTO Logement (desc, adress, budget, type, photo) VALUES ("${desc}", "${adress}", "${budget}", "${type}", "${photo}");`;
+  const query = `INSERT INTO Property (desc, adress, budget, type, photo) VALUES ("${desc}", "${adress}", "${budget}", "${type}", "${photo}");`;
   con.query(query, (err, result) => {
     if (err) throw err;
     console.log("Property registered succesfully");
