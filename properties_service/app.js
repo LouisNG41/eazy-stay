1e3bc2052d37284030c90eff20838293b049d7f7
Bastien <bastien@lacour75.com>
2024-05-18 16:28:35 +0200

    Add routes.js and controller.js for user and properties

--- properties_service/app.js
+++ properties_service/app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 8001;
 const con = require("../config/connection");
 const bodyParser = require("body-parser");
+const propertyRoutes = require("./routes");
 
 // Middleware
 app.use(
@@ -13,6 +14,7 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(propertyRoutes);
 
 con.connect((err) => {
   if (err) {
@@ -25,27 +27,3 @@ con.connect((err) => {
   });
 });
 
-app.get("/properties", (req, res) => {
-  const query = "SELECT * from Property;";
-  con.query(query, (err, result) => {
-    if (err) {
-      console.error("Error executing query:", err.stack);
-      return res.status(500).send("Internal server error.");
-    }
-    res.status(200).send(result);
-  });
-});
-
-app.post("/add-property", (req, res) => {
-  const { desc, adress, budget, type, photo } = req.body;
-  const query = `INSERT INTO Property (desc, adress, budget, type, photo) VALUES ("${desc}", "${adress}", "${budget}", "${type}", "${photo}");`;
-  con.query(query, (err, result) => {
-    if (err) {
-      console.error("Error executing query:", err.stack);
-      return res.status(500).send("Internal server error.");
-    }
-
-    console.log("Property registered successfully");
-    res.status(200).send("Property registered successfully");
-  });
-});
